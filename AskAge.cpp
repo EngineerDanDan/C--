8b2991667ffd75fd82e75b37ec2f373c5c18b7c5
ramneli199 <ramnelimanese27@gmail.com>
2025-06-05 00:44:35 -0500

    Add files via upload

---
+++ AskAge.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main () {
+	
+	int myAge;
+	
+	cout << "What is your age? ";
+	cin >> myAge;
+	
+	if (myAge >= 18) {
+		cout << "Wow legal age";
+	}
+	else if {
+		cout << "Minor";
+	}	
+	else {
+		cout << "Invalid."
+	}
+	return 0;
+}
